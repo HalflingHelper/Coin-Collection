cc5e021bc4525bd81329c44f9f52469fac25af45
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-03-10 10:05:39 -0500

    A single comment

--- models/coin.js
+++ models/coin.js
@@ -5,6 +5,7 @@ const coinSchema = new mongoose.Schema({
     year: { required: true, type: Number },
     design: { type: String, default: 'none' },
     mint: { required: true, type: String, enum: ['p', 's', 'd', 'none']}
+    // Possibly add a condition field (i.e. mint, good, poor...)
 });
 
 module.exports = mongoose.model('Coin', coinSchema)
